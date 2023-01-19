e4d0ab48a13af18adc8d1665144e9972354a71b0
Utka1239 <samsitov@list.ru>
2023-01-19 15:19:22 +0500

    add HomeWork

---
+++ HomeWork/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 2:
+Напишите программу, которая на 
+вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Write("Введите первое число: ");
+string numberStr = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr);
+
+Console.Write("Введите второе число: ");
+string numberStr1 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr1);
+
+
+if (number1 > number2){
+    Console.Write(number1  + " большее, а " + number2   + " меньшее" );
+}
+else{
+    Console.Write(number2  + " большее, а " + number1  + " меньшее" );
+}
---
+++ HomeWork/Task_4/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 4: 
+Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите 1 число: ");
+string numberStr = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr);
+
+Console.Write("Введите 2 число: ");
+string numberStr1 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr1);
+
+Console.Write("Введите 3 число: ");
+string numberStr2 = Console.ReadLine();
+int number3 = Convert.ToInt32(numberStr2);
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine("Максимальное число: " + number1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + number3);
+    }
+}
+
+else if (number2 > number3)
+{
+    Console.WriteLine("Максимальное число: " + number2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + number3);
+}
