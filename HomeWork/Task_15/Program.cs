7c9f03ef9de345d999f51762ee3d51a10ee0314a
Utka1239 <samsitov@list.ru>
2023-01-24 12:01:42 +0500

    add Task_10, 13, 15

---
+++ HomeWork/Task_15/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите цифру от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.Write("Нет");
+}
+else if (number == 2)
+{
+    Console.Write("Нет");
+}
+else if (number == 3)
+{
+    Console.Write("Нет");
+}
+else if (number == 4)
+{
+    Console.Write("Нет");
+}
+else if (number == 5)
+{
+    Console.Write("Нет");
+}
+else if (number == 6)
+{
+    Console.Write("Да");
+}
+else if (number == 7)
+{
+    Console.Write("Да");
+}
+else 
+{
+    Console.Write("Такого дня недели не существсует.");
+}
